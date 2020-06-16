76f3782531ac06adee510f9ce2e3e930b6ddd45b
hendrijanuri <hendrijanuri@gmail.com>
2020-06-17 04:20:45 +0700

    fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,76 @@
 import React, {useState, useEffect} from 'react';
 
-function App() {
+const App = () => {
   // state
-  const [count, setCount] = useState(0)
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=react');
+  const [loading, setLoading] = useState(false);
 
-  const increment = () => {
-      setCount(count+1)
+  // fetch news
+  const fetchNews = () => {
+    // clear news data every fetch
+    setNews([])
+    // set loading true
+    setLoading(true)
+    // fetch api
+    fetch(url)
+    .then(result => result.json())
+    .then(data => (setNews(data.hits), setLoading(false)))
+    .catch(err => console.log(err))
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, [url]);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
   };
 
   return (
     <div>
-      <h2>counter app</h2>
-      <button onClick={increment}>
-        Clicked {count} times
-      </button>
+      <h2>News</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
+      {loading ? "Loading..." : ""}
+      {news.map((n, i) => (<p key={i}>{n.title}</p>))}
     </div>
-  );
-};
+  )
+}
 
 export default App;
+
+
+// COUNTER CLICK APP BELOW 
+
+// function App() {
+//   // state
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     document.title = `Clicked ${count} times`;
+//   });
+
+//   const increment = () => {
+//       setCount(count+1)
+//   };
+
+//   return (
+//     <div>
+//       <h2>counter app</h2>
+//       <button onClick={increment}>
+//         Clicked {count} times
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default App;
