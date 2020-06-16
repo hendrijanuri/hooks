fa8b3246a5e1238896ccc118876f1a194b90865e
hendrijanuri <hendrijanuri@gmail.com>
2020-06-17 04:42:12 +0700

    title

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    document.title = `Search result for "${searchQuery}"`
+    document.title = `Search result for "${searchQuery}"`;
     fetchNews();
   }, [url]);
 
